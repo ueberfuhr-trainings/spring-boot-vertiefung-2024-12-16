4e5ff4f69ced62f0c5b1f563f7944ba66e6c716e
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-12-02 11:46:13 +0100

    Initial commit

---
+++ src/main/java/de/schulung/spring/accounts/AccountApiProviderApplication.java
@@ -0,0 +1,13 @@
+package de.schulung.spring.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApiProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AccountApiProviderApplication.class, args);
+  }
+
+}
