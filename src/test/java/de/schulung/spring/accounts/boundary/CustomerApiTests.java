42110f887c1c38e214dafbde41d325f5424966e8
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-12-06 05:43:37 +0100

    Add PUT /customers/{id} incl. tests.

--- src/test/java/de/schulung/spring/accounts/boundary/CustomerApiTests.java
+++ src/test/java/de/schulung/spring/accounts/boundary/CustomerApiTests.java
@@ -13,6 +13,7 @@ import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -195,5 +196,99 @@ class CustomerApiTests {
 
   }
 
+  @Test
+  void shouldUpdateAndReturnCustomer() throws Exception {
+    // first, we need to create a customer
+    var location = mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+                        {
+                          "name": "Tom Mayer",
+                          "birthdate": "1985-07-03",
+                          "state": "active"
+                        }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andReturn()
+      .getResponse()
+      .getHeader("Location");
+
+    assertThat(location).isNotBlank();
+
+    mockMvc
+      .perform(
+        put(location)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+                        {
+                          "name": "Julia Mayer",
+                          "birthdate": "1984-07-03",
+                          "state": "disabled"
+                        }
+            """)
+      )
+      .andExpect(status().isNoContent());
+
+    mockMvc
+      .perform(
+        get(location)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.name").value("Julia Mayer"))
+      .andExpect(jsonPath("$.birthdate").value("1984-07-03"))
+      .andExpect(jsonPath("$.state").value("disabled"));
+
+  }
+
+  @Test
+  void shouldReturn404OnUpdateMissingCustomer() throws Exception {
+    // first, we need to create a customer
+    var location = mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+                        {
+                          "name": "Tom Mayer",
+                          "birthdate": "1985-07-03",
+                          "state": "active"
+                        }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andReturn()
+      .getResponse()
+      .getHeader("Location");
+
+    assertThat(location).isNotBlank();
+
+    // Now, we delete it
+    mockMvc.perform(
+        delete(location)
+      )
+      .andExpect(status().isNoContent());
+
+    // Not, we can expect 404
+    mockMvc
+      .perform(
+        put(location)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+                        {
+                          "name": "Julia Mayer",
+                          "birthdate": "1984-07-03",
+                          "state": "disabled"
+                        }
+            """)
+      )
+      .andExpect(status().isNotFound());
+
+  }
 
 }
